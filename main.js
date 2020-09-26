6beb453495771f82b1e6b57d6b9dc9b2415cb107
luciferRK <likhithr.cs17@sahyadri.edu.in>
2020-09-26 18:29:54 +0530

    '7'

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 function load(){
 	if(window.tronWeb){
 		tronWeb=window.tronWeb;
-		sessionStorage.setItem('tronobj',JSON.stringify(tronWeb));
+		//sessionStorage.setItem('tronobj',JSON.stringify(tronWeb));
 	}
 	else{
-		tronWeb=JSON.parse(sessionStorage.tronobj);
+console.log("LocalStorage");
+		//tronWeb=JSON.parse(sessionStorage.tronobj);
 	}
 	console.log(tronWeb);
 }
